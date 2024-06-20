03275eef49f39aeea250fe37b09d41f51349361d
Gabriel-Rangel <gsrangel6@gmail.com>
2024-06-20 17:17:35 -0300

    refactoring consumer api

--- src/app/swapi_api_consumer.py
+++ src/app/swapi_api_consumer.py
@@ -1,9 +1,36 @@
 import requests
+from requests import Request
+from typing import Type
+from collections import namedtuple
+
 
 class SwapiApiConsumer:
-    @classmethod
+    def __init__(self) -> None:
+        self.get_starships_response = namedtuple('GET_Starships', 'status_code request response')
+    
     def get_starships(self, page:int) -> any:
-        params = {'page':page}
-        response = requests.get('https://swapi.dev/api/starships/', params=params)
+        # params = {'page':page}
+        # response = requests.get('https://swapi.dev/api/starships/', params=params)
+        # return response.json()
+        req = requests.Request(
+            method='GET', 
+            url='https://swapi.dev/api/starships/', 
+            params={'page': page}
+            )
+        
+        req_prepared = req.prepare()
+        response = self.__send_request(req_prepared)
+        
+        return self.get_starships_response(
+            status_code=response.status_code,
+            request=req,
+            response=response.json()
+        )
 
-        return response.json()
+
+    @classmethod
+    def __send_request(cls, req_prepared:Type[Request]) -> any:
+        http_request = requests.Session()
+        response = http_request.send(req_prepared)
+
+        return response
--- src/tests/test_swapi_api_consumer.py
+++ src/tests/test_swapi_api_consumer.py
@@ -1,9 +1,21 @@
 from app.swapi_api_consumer import SwapiApiConsumer
 
-def test_get_starships(requests_mock):
+def test_get_starships():
 
-    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some':'thing'})
+    #requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some':'thing'})
     swapi = SwapiApiConsumer()
-    response = swapi.get_starships(page=1)
+    page = 1
+    
+    response = swapi.get_starships(page=page)
+
+    assert response.request.method == 'GET'
+    assert response.request.url == 'https://swapi.dev/api/starships/'
+    assert response.status_code == 200
+
+    # req = requests.Request(
+    # method='GET', 
+    # url='https://swapi.dev/api/starships/', 
+    # params={'page': page}
+    # )
 
     print(response)
